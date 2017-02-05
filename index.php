9c9e1d5b13a41c3daa71576bf40cf2b14631b3eb
Patryk11441 <margo08@onet.pl>
2017-02-05 23:37:55 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE HTML>
+<html>
+	<head lang="pl">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Patryk Powalski - Porfolio</title>
+		<link href="style.css" type="text/css" rel="stylesheet">
+		<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
+		<link href="css/fontello.css" type="text/css" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Oswald:700|Roboto+Condensed:700" rel="stylesheet">
+	</head>
+	<body>
+		<nav class="navi navbar navbar-default hide" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Rozwiń menu</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+			 
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<div class="container">
+					<a class="navbar-brand" href="#">Patryk Powalski</a>
+					<ul class="nav navbar-nav navbar-right">
+							<li><a id="ho"  href="#home">Home</a></li>
+							<li><a id="abm" href="#about-me">O nas</a></li>
+							<li><a id="of"  href="#offer">Oferta</a></li>
+							<li><a id="con" href="#contact">Kontakt</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<section class="section" id="home">
+			<div class="container center-p">
+				<div class="title">Patryk Powalski</div>
+				<div class="title title-s">Tworzenie nowoczesnych witryn internetowych</div>
+				<a id="ofb" href="#offer"><button class="bt-look">Przejdz do oferty</button></a>
+			</div>
+		</section>
+		<section class="section" id="about-me">
+			<div class="container">
+				<div class="team">NASZ ZESPÓŁ</div>
+				<div class="col-md-6">
+					<img src="http://bitstreet.in/wp-content/uploads/2016/02/Shahbaz_HashmiJunior-Software-Developer.jpg" class="img-circle img-responsive center" style="margin-bottom: 15px;">
+					<div class="description">Janusz Kowalski - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed odio non est porta aliquet ac sit amet turpis. Nullam a sapien luctus, vulputate velit in, dapibus ligula. Sed efficitur commodo justo. Nunc et ante arcu. Nam risus quam, pellentesque sed odio ut, vehicula tristique dolor. Ut in sagittis odio. Aenean nec porta arcu. Duis at eleifend nunc, a interdum dui.</div>
+				</div>
+				<div class="col-md-6">
+					<img src="http://probusinessinsights.com/wp-content/uploads/2014/08/Press-Release-Expert-PR-Software-Developer-J-Barry-Mandel-300.jpg" class="img-circle img-responsive center" style="margin-bottom: 15px;">
+					<div class="description">Janusz Nowak - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed odio non est porta aliquet ac sit amet turpis. Nullam a sapien luctus, vulputate velit in, dapibus ligula. Sed efficitur commodo justo. Nunc et ante arcu. Nam risus quam, pellentesque sed odio ut, vehicula tristique dolor. Ut in sagittis odio. Aenean nec porta arcu. Duis at eleifend nunc, a interdum dui.</div>
+				</div>
+			</div>
+		</section>
+		<section class="section" id="offer">
+			<div class="container">
+				<div class="team">OFERTA</div>
+				<div class="col-md-4">
+					<img src="img/tlo.png" class="img-circle img-responsive center" style="margin-bottom: 15px;background-color: #f0f0f0;width: 200px;">
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed odio non est porta aliquet ac sit amet turpis. Nullam a sapien luctus, vulputate velit in, dapibus ligula. Sed efficitur commodo justo. Nunc et ante arcu. Nam risus quam, pellentesque sed odio ut, vehicula tristique dolor. Ut in sagittis odio. Aenean nec porta arcu. Duis at eleifend nunc, a interdum dui.</div>
+				</div>
+				<div class="col-md-4">
+					<img src="http://www.freepngimg.com/thumb/responsive_web_design/2-2-responsive-web-design-free-png-image-thumb.png" class="img-circle img-responsive center" style="margin-bottom: 15px;background-color: #f0f0f0;">
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed odio non est porta aliquet ac sit amet turpis. Nullam a sapien luctus, vulputate velit in, dapibus ligula. Sed efficitur commodo justo. Nunc et ante arcu. Nam risus quam, pellentesque sed odio ut, vehicula tristique dolor. Ut in sagittis odio. Aenean nec porta arcu. Duis at eleifend nunc, a interdum dui.</div>
+				</div>
+				<div class="col-md-4">
+					<img src="img/save.png" class="img-circle img-responsive center" style="margin-bottom: 15px;background-color: #f0f0f0;">
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed odio non est porta aliquet ac sit amet turpis. Nullam a sapien luctus, vulputate velit in, dapibus ligula. Sed efficitur commodo justo. Nunc et ante arcu. Nam risus quam, pellentesque sed odio ut, vehicula tristique dolor. Ut in sagittis odio. Aenean nec porta arcu. Duis at eleifend nunc, a interdum dui.</div>
+				</div>
+			</div>
+		</section>
+		<section class="section" id="porfolio">
+			<div class="container">
+			</div>
+		</section>
+		<section class="section" id="contact">
+			<div class="container">
+				<div class="contact">KONTAKT</div>
+				<div class="col-md-6">
+					<input class="col-md-12 col-sm-12 col-xs-12 type-text" type="text" placeholder="Twoje imię i nazwisko">
+					<input class="col-md-12 col-sm-12 col-xs-12 type-text" type="text" placeholder="Twój e-mail">
+					<textarea class="col-md-12 col-sm-12 col-xs-12 type-text" style="resize: none; height: 240px;"placeholder="Treść wiadomości"></textarea>
+					<input class="col-md-12 col-sm-12 col-xs-12 type-submit" type="submit" value="Wyślij">
+				</div>
+				<div class="col-md-6 cont">
+					Patryk Powalski Sp. z o.o. <br>
+					Adres: ul. Wołczyńska 37, 60-003 Poznań <br>
+					Telefon: +48 61 668 07 45 <br>
+					Faks: +48 61 668 00 09 <br>
+					Kapitał zakładowy 5 010 000 PLN w pełni opłacony
+				</div>
+			</div>
+		</section>
+		<footer class="footer">Copyright &copy; 2017 by Patryk Powalski| All right reserved</footer>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<script src="js/jquery.scrollTo.min.js"></script>
+		<script>
+			$(document).ready(function() {
+				var PosY = $('#ofb').offset().top;
+				var sticky = function()
+				{
+					var PosScroll = $(window).scrollTop();
+					
+					if(PosScroll > PosY)
+					{
+						$('.navi').addClass('sticky');
+						$('.navi').removeClass('hide');
+						
+					} else {
+						$('.navi').removeClass('sticky');
+						$('.navi').addClass('hide');
+					}
+				};
+				
+				sticky();
+				
+				$(window).scroll(function() {
+					sticky();
+				});
+				
+			});
+			
+			jQuery(function($)
+			{
+				$.scrollTo(0);
+				$('#ho').click(function() {$.scrollTo($('#home'), 500);});
+				$('#ofb').click(function() {$.scrollTo($('#offer'), 500);});
+				$('#abm').click(function() {$.scrollTo($('#about-me'), 500);});
+				$('#con').click(function() {$.scrollTo($('#contact'), 500);});
+				$('#of').click(function() {$.scrollTo($('#offer'), 500);});
+			});
+		</script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
